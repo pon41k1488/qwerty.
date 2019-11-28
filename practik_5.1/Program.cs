5ad7d31cdd8a2d30b8d25964bb641881cfa11e3a
Вова Миронов <pon41k@MacBook-Pro-Vova.local>
2019-11-28 10:58:49 +0300

    ноябрь

---
+++ practik_5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace practik_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = Console.ReadLine();
+
+            string z = "";
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int e = 1; e <= n; e++)
+            {
+                z += '*';
+            }
+
+            for (int i = 0; i <= stroka.Length; i++)
+            {
+                Console.Write(stroka[i] + z);
+            }
+        }
+    }
+}
